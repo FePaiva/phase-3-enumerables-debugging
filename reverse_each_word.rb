0645a084a4337c5963594cc5beb19ed172c27277
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-12 14:08:11 -0800

    commit tree 48ce6ef356a837c06774613c1a3c3d76996216fb

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,30 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+  # binding.pry
+words = sentence.split
+reversed_words = []
+
+words.each do |word|
+  # binding.pry
+  reversed_words << word.reverse
+end
+# binding.pry
+reversed_words.join('')
+end
+
+# binding.pry
+
+# 0
+
+# puts reverse_each_word("Hello there, and how are you?")
+
+#  fazendo com map
+
+# reversed_words = words.map do |word|
+#   word.reverse
+# end
+
+# reversed_words.join(" ")
+# end
